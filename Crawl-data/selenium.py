dd190ef37493e4a560526b79f4e34d4214bd3e06
BichTram1108 <19522375@gm.uit.edu.vn>
2023-02-02 16:01:08 +0700

    create folder Crawl-data
    add file

---
+++ Crawl-data/selenium.py
@@ -0,0 +1,9 @@
+import numpy as np
+from selenium import webdriver
+from time import sleep
+import random
+from selenium.common.exceptions import NoSuchElementException, ElêmntNotInteractableException
+from selenium.webdriver.common.by import By
+import pandas as pd
+
+driver = webdriver.Cho
